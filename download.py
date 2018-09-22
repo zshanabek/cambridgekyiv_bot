4a8844f1bcb0e20fcc0a67e1485bcac6ba4f6d6f
Zhunissali Shanabek <zshanabek@gmail.com>
2018-09-23 01:48:58 +0300

    [Add] - delete and download file

--- download.py
+++ download.py
@@ -1,16 +1,28 @@
 import youtube_dl
+import traceback
+import pdb
+import os
 
 
-options = {
-  'format': 'bestaudio/best',
-  'extractaudio' : True,
-  'audioformat' : "mp3",
-  'outtmpl': '%(id)s',
-  'noplaylist' : True
-}
+def		get_video_info(url):
+	ydl = youtube_dl.YoutubeDL()
+	try:
+		with ydl:
+			r = ydl.extract_info(url, download=False)
+		a = '{0} uploaded by {1}, has {2} views, {3} likes, and {4} dislikes'.format(r['title'], r['uploader'], r['view_count'], r['like_count'], r['dislike_count'])
+		return a
+	except Exception as e:
+		return None
 
-def		get_video_info(url)
-	r = None
+def		get_audio(url):
+	options = {
+		'format': 'bestaudio/best', # choice of quality
+		'extractaudio' : True,      # only keep the audio
+		'audioformat' : "mp3",      # convert to mp3 
+		'outtmpl': '%(id)s',        # name the file the ID of the video
+		'noplaylist' : True
+	}
 	ydl = youtube_dl.YoutubeDL(options)
-	with ydl:
-		r = ydl.extract_info(url, download=False) 
+	result = ydl.extract_info(url, download=True)
+	os.rename(result['id'], "{}.mp3".format(result['title']))
+	return result['title']
--- start.py
+++ start.py
@@ -2,27 +2,41 @@ import config
 import telebot
 import os
 import download
+import pdb
+import logging
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
-
-# Handle '/start' and '/help'
+logger = telebot.logger
+# telebot.logger.setLevel(logging.DEBUG)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Hi there, I am YoutubeBot.")
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
 	chat_id = message.chat.id
-	r = None
 	url = message.text
-	with ydl:
-		r = ydl.extract_info(url, download=False) 
-	audio = open('Rohirrim Charge (Battle of the Pelennor Fields) HD-l8yOdAqBFcQ.mp3', 'rb')
-	bot.send_audio(chat_id, audio)
-	os.remove('Rohirrim Charge (Battle of the Pelennor Fields) HD-l8yOdAqBFcQ.mp3')
+	a =	download.get_video_info(url)
+	if a == None:
+		bot.reply_to(message, "Can't extract audio info")
+	else:
+		keyboard = types.InlineKeyboardMarkup()
+		yes = types.InlineKeyboardButton(text="Download", callback_data = 'yes ' + url)
+		no = types.InlineKeyboardButton(text="Cancel", callback_data = 'no ')
+		keyboard.add(yes, no)
+		bot.send_message(chat_id, a, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+	chat_id = call.message.chat.id
+	if call.message:
+		a = call.data.split()
+		if a[0] == 'yes':
+			a = download.get_audio(a[1])
+			audio = open('{0}.mp3'.format(a), 'rb')
+			bot.send_audio(chat_id, audio)
+			os.remove('{}.mp3'.format(a))
+		else:
+			pass
 bot.polling()
